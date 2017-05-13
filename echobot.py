b19f8e14f8a6b456326223be21874f0144027199
Luthfi Kurnia Putra <luthfiswees@gmail.com>
2017-05-14 03:51:46 +0700

    revert updater back to normal

--- echobot.py
+++ echobot.py
@@ -84,10 +84,7 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_webhook(listen="0.0.0.0",
-                      port=PORT,
-                      url_path=TOKEN)
-    updater.bot.set_webhook("https://inget-skripsi.herokuapp.com/" + TOKEN)
+    updater.start_polling()
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
